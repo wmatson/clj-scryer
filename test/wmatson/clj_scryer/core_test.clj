ead41bdac5c31515cc9c3e7ca3e9aa06b006d5f6
wmatson <wmatson@live.com>
2018-11-28 22:44:26 -0700

    Removing unused test file

--- test/wmatson/clj_scryer/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns wmatson.clj-scryer.core-test
-  (:require [clojure.test :refer :all]
-            [wmatson.clj-scryer.swing :refer :all]))
-
-(deftest a-testr
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
