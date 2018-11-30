8bbfeeda19dcd4f0d5fafc7261640f434c832357
wmatson <wmatson@live.com>
2018-11-29 17:26:49 -0700

    Basic hiccup-rendered browser inspector

--- project.clj
+++ project.clj
@@ -5,4 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [cider/orchard "0.3.3"]
-                 [hiccup "1.0.5"]])
+                 [hiccup "1.0.5"]
+                 [ring-server "0.5.0"]])
--- src/wmatson/clj_scryer/hiccup.clj
+++ src/wmatson/clj_scryer/hiccup.clj
@@ -1,13 +1,21 @@
 (ns wmatson.clj-scryer.hiccup
   (:require [hiccup.core :as hccp]
-            [orchard.inspect :as ori]))
+            [orchard.inspect :as ori]
+            [ring.server.standalone :as server]))
+
+(defonce ^:private current-inspector (atom (ori/fresh)))
+(defonce ^:private server (atom nil))
 
 (defn- concat-line-strings [line]
   (->> line
        (partition-by string?)
        (map-indexed #(if (even? %1)
                        (apply str %2)
-                       (apply concat %2)))))
+                       (apply concat %2)))
+       (remove #(and (string? %) (clojure.string/blank? %)))))
+
+(defn- command-button [command text & [param]]
+  [:a.button {:href (str "/" command "/" param)} text])
 
 (defn- iline->hiccup [line]
   (->> line
@@ -16,13 +24,16 @@
                  (conj acc (if (#{:value} k)
                              [:span
                               [:span.is-family-monospace display]
-                              [:button.button "&gt;"]]
+                              (command-button "down" "&gt;" idx)]
                              [:span.tag next])))
                [:div])))
 
 (defn- irender->hiccup [inspector-rendered]
   (conj [:div.container
-         [:div.level [:button "Prev"] [:button "Up"] [:button "Next"]]]
+         [:div.level
+          (command-button "prev" "Prev")
+          (command-button "up" "Up")
+          (command-button "next" "Next")]]
         (->> inspector-rendered
              (partition-by (comp #{:newline} first))
              (partition-all 1 2)
@@ -35,11 +46,29 @@
     [:link {:rel "stylesheet" :href "https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.2/css/bulma.min.css"}]]
    [:body hiccup]])
 
+(defn render-page [inspector]
+  (-> inspector :rendered irender->hiccup surround-in-page hccp/html))
+
+(defn handler [{:keys [uri]}]
+  (when-let [[_ command param] (re-find #"^/([^/]+)/(.+)?$" uri)]
+    (condp = command
+      "down" (swap! current-inspector ori/down (Integer/parseInt param))
+      "up" (swap! current-inspector ori/up)
+      "prev" (swap! current-inspector ori/prev-page)
+      "next" (swap! current-inspector ori/next-page)
+      :ignore-otherwise))
+  {:status 200 :body (render-page @current-inspector)})
+
 (defn inspect [value]
-  (let [hiccup (irender->hiccup (:rendered (ori/start (ori/fresh) value)))
-        temp-file (java.io.File/createTempFile "inspect" ".html")]
-    (spit temp-file (hccp/html (surround-in-page hiccup)))
-    (clojure.java.browse/browse-url (.. temp-file toURI toString))))
+  (swap! current-inspector ori/start value)
+  (when-not @server
+    (reset! server (server/serve handler {:port (Integer/getInteger "wmatson.scryer.hiccup.port" 31415)
+                                          :join? false
+                                          :open-browser? false})))
+  (let [port (-> @server .getConnectors (aget 0) .getLocalPort)
+        url (str "http://localhost:" port "/")]
+    (clojure.java.browse/browse-url url)))
 
 (comment
-  (inspect (map #(hash-map :num %) (range))))
+  (do (swap! server #(.stop %))
+      (inspect (map #(hash-map :num %) (range)))))
